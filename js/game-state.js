fd67903f69be20658125664e6b0399dcb389e106
kjk3217 <kjk3217@naver.com>
2025-10-20 09:02:51 +0900

    Update game-state.js

--- js/game-state.js
+++ js/game-state.js
@@ -31,7 +31,8 @@ let puzzleInitialParent = null;
 const transitionVideos = {
     start: 'videos/start_to_room1.mp4',
     room1: 'videos/room1_to_room2.mp4',
-    room2: 'videos/room2_to_room3.mp4'
+    room2: 'videos/room2_to_room3.mp4',
+    ending: 'videos/room3_to_ending.mp4'
 };
 
 // 퀴즈 데이터
@@ -160,6 +161,7 @@ function getRoomQuizzes(roomNum) {
     }
 
 }
+
 
 
 
